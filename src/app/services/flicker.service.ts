b38d760c94c3f33cb8901beb9f6e14053b02504c
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-05-19 12:43:42 +0200

    Add: Date Taken and limit per_page to be 1

--- src/app/services/flicker.service.ts
+++ src/app/services/flicker.service.ts
@@ -7,11 +7,14 @@ import { map } from "rxjs/operators";
 export class FlickrService {
     result$: Observable<any>;
     key = '9d36c7156cedeb140ecaad40970112f2';
+    per_page = 1;
+    sort = 'interestingness-desc';
+
     constructor(private _http: Http) { };
 
     getResult(query: string) {
 
-        let url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=${query}&per_page=12&format=json&nojsoncallback=1`;
+        let url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.key}&tags=${query}&extras=date_taken&per_page=${this.per_page}&sort=${this.sort}&format=json&nojsoncallback=1`;
 
         return this._http
             .get(url)
@@ -19,17 +22,18 @@ export class FlickrService {
             .pipe(map((val) => {
                 if (val.stat === 'ok') {
                     return val.photos.photo.map((photo: any) => {
+                        console.log(photo)
                         return {
                             url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`,
                             title: photo.title,
-                            date_taken: photo.date_taken
+                            datetaken: photo.datetaken
                         }
                     })
                 }
                 else {
-                    return [];
+                    return [
+                    ];
                 }
             }));
     }
 }
-
