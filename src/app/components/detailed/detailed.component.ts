d2b9374666f203c27fa30a3499c756268bad0bfe
Omar AbdEl-Hady <omar@omarabdelhady.me>
2018-05-19 13:46:33 +0200

    Add: Detailed view component 'Service call incomplete'

--- src/app/components/detailed/detailed.component.ts
+++ src/app/components/detailed/detailed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { FlickrService } from '../../services/flicker.service';
 
 @Component({
   selector: 'app-detailed',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailedComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  model$: Observable<any>;
+  photos: Object;
+  constructor(private _formBuilder: FormBuilder, private _flickrService: FlickrService) {
   }
+  
+  ngOnInit() {
+    // Make another GET function in Service
 
+    // EventEmitter for Tag value
+
+    // Pagination
+
+
+
+    // this.tag.pipe(switchMap((query: string) => this._flickrService.getAllResults(query)))
+    //   .subscribe(value => {
+    //     this.photos = value;
+    //   });
+  }
 }
